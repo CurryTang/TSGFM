2f678cb4bed5e6850e836060c391d35af86911d2
CurryTang <jjoo1997@hotmail.com>
2024-04-24 14:00:35 -0400

    hpcc

---
+++ linkpred.py
@@ -0,0 +1,252 @@
+"""
+main module
+"""
+import argparse
+import time
+import warnings
+from math import inf
+import sys
+import random
+
+sys.path.insert(0, '..')
+
+import numpy as np
+import torch
+from ogb.linkproppred import Evaluator
+
+torch.set_printoptions(precision=4)
+import wandb
+# when generating subgraphs the supervision edge is deleted, which triggers a SparseEfficiencyWarning, but this is
+# not a performance bottleneck, so suppress for now
+from scipy.sparse import SparseEfficiencyWarning
+
+warnings.filterwarnings("ignore", category=SparseEfficiencyWarning)
+
+from link.data import get_data, get_loaders
+from models.elph import ELPH, BUDDY
+from link.utils import ROOT_DIR, print_model_params, select_embedding, str2bool
+from link.train import get_train_func
+from link.inference import test
+
+def print_results_list(results_list):
+    for idx, res in enumerate(results_list):
+        print(f'repetition {idx}: test {res[0]:.2f}, val {res[1]:.2f}, train {res[2]:.2f}')
+
+def set_seed(seed):
+    """
+    setting a random seed for reproducibility and in accordance with OGB rules
+    @param seed: an integer seed
+    @return: None
+    """
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def run(args):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"executing on {device}")
+    results_list = []
+    train_func = get_train_func(args)
+    for rep in range(args.reps):
+        set_seed(rep)
+        dataset, splits, directed, eval_metric = get_data(args)
+        train_loader, train_eval_loader, val_loader, test_loader = get_loaders(args, dataset, splits, directed)
+        if args.dataset_name.startswith('ogbl'):  # then this is one of the ogb link prediction datasets
+            evaluator = Evaluator(name=args.dataset_name)
+        else:
+            evaluator = Evaluator(name='ogbl-ppa')  # this sets HR@100 as the metric
+        emb = select_embedding(args, dataset.data.num_nodes, device)
+        model, optimizer = select_model(args, dataset, emb, device)
+        val_res = test_res = best_epoch = 0
+        print(f'running repetition {rep}')
+        if rep == 0:
+            print_model_params(model)
+        for epoch in range(args.epochs):
+            t0 = time.time()
+            loss = train_func(model, optimizer, train_loader, args, device)
+            if (epoch + 1) % args.eval_steps == 0:
+                results = test(model, evaluator, train_eval_loader, val_loader, test_loader, args, device,
+                               eval_metric=eval_metric)
+                for key, result in results.items():
+                    train_res, tmp_val_res, tmp_test_res = result
+                    if tmp_val_res > val_res:
+                        val_res = tmp_val_res
+                        test_res = tmp_test_res
+                        best_epoch = epoch
+                    res_dic = {f'rep{rep}_loss': loss, f'rep{rep}_Train' + key: 100 * train_res,
+                               f'rep{rep}_Val' + key: 100 * val_res, f'rep{rep}_tmp_val' + key: 100 * tmp_val_res,
+                               f'rep{rep}_tmp_test' + key: 100 * tmp_test_res,
+                               f'rep{rep}_Test' + key: 100 * test_res, f'rep{rep}_best_epoch': best_epoch,
+                               f'rep{rep}_epoch_time': time.time() - t0, 'epoch_step': epoch}
+                    if args.wandb:
+                        wandb.log(res_dic)
+                    to_print = f'Epoch: {epoch:02d}, Best epoch: {best_epoch}, Loss: {loss:.4f}, Train: {100 * train_res:.2f}%, Valid: ' \
+                               f'{100 * val_res:.2f}%, Test: {100 * test_res:.2f}%, epoch time: {time.time() - t0:.1f}'
+                    print(key)
+                    print(to_print)
+        if args.reps > 1:
+            results_list.append([test_res, val_res, train_res])
+            print_results_list(results_list)
+    if args.reps > 1:
+        test_acc_mean, val_acc_mean, train_acc_mean = np.mean(results_list, axis=0) * 100
+        test_acc_std = np.sqrt(np.var(results_list, axis=0)[0]) * 100
+        val_acc_std = np.sqrt(np.var(results_list, axis=0)[1]) * 100
+
+        wandb_results = {'test_mean': test_acc_mean, 'val_mean': val_acc_mean, 'train_mean': train_acc_mean,
+                         'test_acc_std': test_acc_std, 'val_acc_std': val_acc_std}
+        print(wandb_results)
+        if args.wandb:
+            wandb.log(wandb_results)
+    if args.wandb:
+        wandb.finish()
+    if args.save_model:
+        path = f'{ROOT_DIR}/saved_models/{args.dataset_name}'
+        torch.save(model.state_dict(), path)
+
+
+def select_model(args, dataset, emb, device):
+    if args.model == 'GCN':
+        pass 
+    elif args.model == 'BUDDY':
+        model = BUDDY(args, dataset.num_features, node_embedding=emb).to(device)
+    elif args.model == 'ELPH':
+        model = ELPH(args, dataset.num_features, node_embedding=emb).to(device)
+    else:
+        raise NotImplementedError
+    parameters = list(model.parameters())
+    if args.train_node_embedding:
+        torch.nn.init.xavier_uniform_(emb.weight)
+        parameters += list(emb.parameters())
+    optimizer = torch.optim.Adam(params=parameters, lr=args.lr, weight_decay=args.weight_decay)
+    total_params = sum(p.numel() for param in parameters for p in param)
+    print(f'Total number of parameters is {total_params}')
+    if args.model == 'DGCNN':
+        print(f'SortPooling k is set to {model.k}')
+    return model, optimizer
+
+
+if __name__ == '__main__':
+    # Data settings
+    parser = argparse.ArgumentParser(description='Co-train link prediction')
+    parser.add_argument("--pre_train_datasets", type=str, nargs='+', default=["cora", "citeseer", "pubmed", "arxiv", "arxiv23"])
+    parser.add_argument("--downstream_datasets", type=str, nargs='+', default=["cora", "citeseer", "pubmed"])
+    parser.add_argument('--val_pct', type=float, default=0.1,
+                        help='the percentage of supervision edges to be used for validation. These edges will not appear'
+                             ' in the training set and will only be used as message passing edges in the test set')
+    parser.add_argument('--test_pct', type=float, default=0.2,
+                        help='the percentage of supervision edges to be used for test. These edges will not appear'
+                             ' in the training or validation sets for either supervision or message passing')
+    parser.add_argument('--train_samples', type=float, default=inf, help='the number of training edges or % if < 1')
+    parser.add_argument('--val_samples', type=float, default=inf, help='the number of val edges or % if < 1')
+    parser.add_argument('--test_samples', type=float, default=inf, help='the number of test edges or % if < 1')
+    parser.add_argument('--preprocessing', type=str, default=None)
+    parser.add_argument('--sign_k', type=int, default=0)
+    parser.add_argument('--load_features', action='store_true', help='load node features from disk')
+    parser.add_argument('--load_hashes', action='store_true', help='load hashes from disk')
+    parser.add_argument('--cache_subgraph_features', action='store_true',
+                        help='write / read subgraph features from disk')
+    parser.add_argument('--train_cache_size', type=int, default=inf, help='the number of training edges to cache')
+    parser.add_argument('--year', type=int, default=0, help='filter training data from before this year')
+    # GNN settings
+    parser.add_argument('--model', type=str, default='BUDDY')
+    parser.add_argument('--hidden_channels', type=int, default=1024)
+    parser.add_argument('--batch_size', type=int, default=1024)
+    parser.add_argument('--eval_batch_size', type=int, default=1000000,
+                        help='eval batch size should be largest the GPU memory can take - the same is not necessarily true at training time')
+    parser.add_argument('--label_dropout', type=float, default=0.5)
+    parser.add_argument('--feature_dropout', type=float, default=0.5)
+    parser.add_argument('--sign_dropout', type=float, default=0.5)
+    parser.add_argument('--save_model', action='store_true', help='save the model to use later for inference')
+    parser.add_argument('--feature_prop', type=str, default='gcn',
+                        help='how to propagate ELPH node features. Values are gcn, residual (resGCN) or cat (jumping knowledge networks)')
+    # SEAL settings
+    parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--num_seal_layers', type=int, default=3)
+    parser.add_argument('--sortpool_k', type=float, default=0.6)
+    parser.add_argument('--label_pooling', type=str, default='add', help='add or mean')
+    parser.add_argument('--seal_pooling', type=str, default='edge', help='how SEAL pools features in the subgraph')
+    # Subgraph settings
+    parser.add_argument('--num_hops', type=int, default=1)
+    parser.add_argument('--ratio_per_hop', type=float, default=1.0)
+    parser.add_argument('--max_nodes_per_hop', type=int, default=None)
+    parser.add_argument('--node_label', type=str, default='drnl')
+    parser.add_argument('--max_dist', type=int, default=4)
+    parser.add_argument('--max_z', type=int, default=1000,
+                        help='the size of the label embedding table. ie. the maximum number of labels possible')
+    parser.add_argument('--use_feature', type=str2bool, default=True,
+                        help="whether to use raw node features as GNN input")
+    parser.add_argument('--use_struct_feature', type=str2bool, default=True,
+                        help="whether to use structural graph features as GNN input")
+    parser.add_argument('--use_edge_weight', action='store_true',
+                        help="whether to consider edge weight in GNN")
+    # Training settings
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--weight_decay', type=float, default=0, help='Weight decay for optimization')
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--num_workers', type=int, default=4)
+    parser.add_argument('--num_negs', type=int, default=1, help='number of negatives for each positive')
+    parser.add_argument('--train_node_embedding', action='store_true',
+                        help="also train free-parameter node embeddings together with GNN")
+    parser.add_argument('--propagate_embeddings', action='store_true',
+                        help='propagate the node embeddings using the GCN diffusion operator')
+    parser.add_argument('--loss', default='bce', type=str, help='bce or auc')
+    parser.add_argument('--add_normed_features', dest='add_normed_features', type=str2bool,
+                        help='Adds a set of features that are normalsied by sqrt(d_i*d_j) to calculate cosine sim')
+    parser.add_argument('--use_RA', type=str2bool, default=False, help='whether to add resource allocation features')
+    # SEAL specific args
+    parser.add_argument('--dynamic_train', action='store_true',
+                        help="dynamically extract enclosing subgraphs on the fly")
+    parser.add_argument('--dynamic_val', action='store_true')
+    parser.add_argument('--dynamic_test', action='store_true')
+    parser.add_argument('--pretrained_node_embedding', type=str, default=None,
+                        help="load pretrained node embeddings as additional node features")
+    # Testing settings
+    parser.add_argument('--reps', type=int, default=1, help='the number of repetition of the experiment to run')
+    parser.add_argument('--use_valedges_as_input', action='store_true')
+    parser.add_argument('--eval_steps', type=int, default=1)
+    parser.add_argument('--log_steps', type=int, default=1)
+    parser.add_argument('--eval_metric', type=str, default='hits',
+                        choices=('hits', 'mrr', 'auc'))
+    parser.add_argument('--K', type=int, default=100, help='the hit rate @K')
+    # hash settings
+    parser.add_argument('--use_zero_one', type=str2bool, default=0,
+                        help="whether to use the counts of (0,1) and (1,0) neighbors")
+    parser.add_argument('--floor_sf', type=str2bool, default=0,
+                        help='the subgraph features represent counts, so should not be negative. If --floor_sf the min is set to 0')
+    parser.add_argument('--hll_p', type=int, default=8, help='the hyperloglog p parameter')
+    parser.add_argument('--minhash_num_perm', type=int, default=128, help='the number of minhash perms')
+    parser.add_argument('--max_hash_hops', type=int, default=2, help='the maximum number of hops to hash')
+    parser.add_argument('--subgraph_feature_batch_size', type=int, default=11000000,
+                        help='the number of edges to use in each batch when calculating subgraph features. '
+                             'Reduce or this or increase system RAM if seeing killed messages for large graphs')
+    # wandb settings
+    parser.add_argument('--wandb', action='store_true', help="flag if logging to wandb")
+    parser.add_argument('--wandb_offline', dest='use_wandb_offline',
+                        action='store_true')  # https://docs.wandb.ai/guides/technical-faq
+
+    parser.add_argument('--wandb_sweep', action='store_true',
+                        help="flag if sweeping")  # if not it picks up params in greed_params
+    parser.add_argument('--wandb_watch_grad', action='store_true', help='allows gradient tracking in train function')
+    parser.add_argument('--wandb_track_grad_flow', action='store_true')
+
+    parser.add_argument('--wandb_entity', default="link-prediction", type=str)
+    parser.add_argument('--wandb_project', default="link-prediction", type=str)
+    parser.add_argument('--wandb_group', default="testing", type=str, help="testing,tuning,eval")
+    parser.add_argument('--wandb_run_name', default=None, type=str)
+    parser.add_argument('--wandb_output_dir', default='./wandb_output',
+                        help='folder to output results, images and model checkpoints')
+    parser.add_argument('--wandb_log_freq', type=int, default=1, help='Frequency to log metrics.')
+    parser.add_argument('--wandb_epoch_list', nargs='+', default=[0, 1, 2, 4, 8, 16],
+                        help='list of epochs to log gradient flow')
+    parser.add_argument('--log_features', action='store_true', help="log feature importance")
+    args = parser.parse_args()
+    if (args.max_hash_hops == 1) and (not args.use_zero_one):
+        print("WARNING: (0,1) feature knock out is not supported for 1 hop. Running with all features")
+    if args.dataset_name == 'ogbl-ddi':
+        args.use_feature = 0  # dataset has no features
+        assert args.sign_k > 0, '--sign_k must be set to > 0 i.e. 1,2 or 3 for ogbl-ddi'
+    print(args)
+    run(args)
---
+++ sslmain.py
@@ -0,0 +1,238 @@
+#CUDA_VISIBLE_DEVICES=0 /remote-home/mzhong/anaconda3/bin/python train.py --subgraph_size 10 --batch_size 200 
+import argparse, os
+import math
+import torch
+import random
+import numpy as np
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+from torch_geometric.nn import global_mean_pool
+from subgcon.efficient_dataset import NodeSubgraphDataset, LinkSubgraphDataset, GraphSubgraphDataset
+from subgcon.utils_mp import Subgraph, preprocess
+from subgcon.models import SugbCon, Encoder, Scorer, Pool
+from graphmae.utils import build_args, create_optimizer, set_random_seed, load_best_configs
+from graphmae.data_util import unify_dataset_loader
+from graphmae.models import build_model, TaskHeadModel
+from graphmae.evaluation import linear_test, link_linear_test
+from graphmae.models.gcn import GNN_node
+import logging
+import os.path as osp
+from tqdm import tqdm
+from graphmae.config import DATASET
+from ogb.linkproppred.evaluate import Evaluator
+import pytorch_warmup as warmup
+
+def train(model, optimizer, loader, args, scheduler, device, model_type='subgcon', mask = None, head_name = None, warmup_scheduler = None):
+    # Model training
+    model.train()
+    optimizer.zero_grad()
+    avg_loss = 0
+    ce_loss = torch.nn.CrossEntropyLoss()
+    for batch in loader:
+        if batch.x.dim() == 1 and args.embed_mode == 'llm':
+            batch.x = batch.node_text_feat
+            batch.xe = batch.edge_text_feat
+        if model_type == 'subgcon':
+            index = batch.ptr[:-1]
+            edge_feat = batch.xe if hasattr(batch, 'xe') else None
+            z, summary = model(batch.x.to(device), batch.edge_index.to(device), batch.batch.to(device), index.to(device), edge_feat)
+            loss = model.loss(z, summary)
+        elif model_type == 'graphmae':
+            loss, loss_item = model(batch.x.to(device), batch.edge_index.to(device))
+        elif model_type == 'cotrain':
+            ## mask and head_name should be provided for this case
+            output = model(batch.x.to(device), batch.edge_index.to(device), head_name)
+            loss = ce_loss(output[mask], batch.y[mask])
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+    if scheduler is not None:
+        if warmup_scheduler is None:
+            scheduler.step()
+        else:
+            with warmup_scheduler.dampening():
+                scheduler.step()
+    avg_loss += loss.item()
+    return avg_loss / len(loader)
+
+def get_all_node_emb(model, num_node, loader, args, device, level='node', modeln='subgcon'):
+    # Obtain central node embs from subgraphs 
+    model.eval()
+    z = []
+    size = len(loader)
+    for batch in loader:
+        if batch.x.dim() == 1 and args.embed_mode == 'llm':
+            batch.x = batch.node_text_feat
+            batch.xe = batch.edge_text_feat
+        index = batch.ptr[:-1]
+        if modeln == 'subgcon':
+            edge_feat = batch.xe if hasattr(batch, 'xe') else None
+            node, graph = model(batch.x.to(device), batch.edge_index.to(device), batch.batch.to(device), index.to(device), edge_feat.to(device))
+        elif modeln == 'graphmae':
+            # import ipdb; ipdb.set_trace()
+            out = model.embed(batch.x.to(device), batch.edge_index.to(device))
+            if size > 1:
+                node = out[index]
+            else:
+                node = out
+            graph = global_mean_pool(out, batch.batch.to(device))
+            # import ipdb; ipdb.set_trace()
+        if level == 'graph':
+            z.append(graph)
+        else:
+            z.append(node)
+    z = torch.cat(z, dim = 0)
+    return z
+    
+    
+def node_level_test(model, dataset, loader, args, device):
+    # Model testing
+    model.eval()
+    num_node = dataset.num_nodes
+    with torch.no_grad():
+        all_emb = get_all_node_emb(model, num_node, loader, args, device, level = 'node', modeln=args.method)
+
+    dataset.data.num_classes = dataset.data.y.max().item() + 1
+    test_acc, estp_test_acc, val_acc = linear_test(all_emb, dataset.data, 100, device)
+    # val_acc, test_acc = model.test(train_z, train_y, val_z, val_y, test_z, test_y)
+    print('val_acc = {} test_acc = {}'.format(val_acc, estp_test_acc))
+    return val_acc, test_acc
+
+def link_level_test(model, dataset, loader, args, device):
+    model.eval()
+    with torch.no_grad():
+        z = get_all_node_emb(model, 1, loader, args, device, level = 'link', modeln=args.method)
+    
+    evaluator = Evaluator(name='ogbl-ppa')
+    dataset.data.num_classes = dataset.data.y.max().item() + 1
+    test_acc, val_acc = link_linear_test(z, dataset.data, 100, device, evaluator)
+    print('val_acc = {} test_acc = {}'.format(val_acc, test_acc))
+    return val_acc, test_acc 
+
+def graph_level_test(model, dataset, loader, args, device):
+    model.eval()
+    with torch.no_grad():
+        z = get_all_node_emb(model, 1, loader, args, device, level = 'graph', modeln=args.method)
+    
+    test_acc, estp_test_acc, val_acc = linear_test(z, dataset, 100, device)
+    print('val_acc = {} test_acc = {}'.format(val_acc, test_acc))
+    return val_acc, test_acc
+
+
+def test(model, dataset, loader, args, device, level='node'):
+    if level == 'node':
+        return node_level_test(model, dataset, loader, args, device)
+    elif level == 'link':
+        return link_level_test(model, dataset, loader, args, device)
+    elif level == 'graph':
+        return graph_level_test(model, dataset, loader, args, device)
+
+
+def main(args):
+    use_scheduler = args.scheduler
+    batch_size = args.batch_size
+    if args.device < 0:
+        device = "cpu"
+    else:
+        device = "cuda:{}".format(args.device)
+    feature_dim = args.feature_dim
+    args.num_features = feature_dim 
+    ## pretrain part
+    max_epochs = args.max_epoch
+    use_scheduler = args.scheduler
+    ## select method
+    if args.method == 'subgcon':
+        if args.backbone == 'gcn_node':
+            model = SugbCon(
+                hidden_channels=args.num_hidden, encoder=Encoder(feature_dim, args.num_hidden),
+                pool=Pool(in_channels=args.num_hidden),
+                scorer=Scorer(args.num_hidden)).to(device)
+        elif args.backbone == 'gcn_graph':
+            model = SugbCon(
+                hidden_channels=args.num_hidden, encoder=GNN_node(args.num_layers, args.num_hidden, args.in_drop, residual=args.residual, gnn_type=args.encoder, embed_mode=args.embed_mode),pool=Pool(in_channels=args.num_hidden),
+                scorer=Scorer(args.num_hidden)).to(device)
+        ## subgcon only supports subgraph
+        args.split_mode = 'subgraph'
+    elif args.method == 'graphmae':
+        model = build_model(args).to(device)
+    elif args.method == 'cotrain':
+        model = build_model(args).to(device)
+    else:
+        raise NotImplementedError("Method {} not implemented".format(args.method))
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    if use_scheduler:
+        if args.lrtype == 'lambda':
+            logging.info("Use scheduler lambdalr")
+            scheduler = lambda epoch :( 1 + np.cos((epoch) * np.pi / max_epochs) ) * 0.5
+            scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=scheduler)
+            if args.warmup:
+                warmup_scheduler = warmup.UntunedLinearWarmup(optimizer)
+            else:
+                warmup_scheduler = None
+        elif args.lrtype == 'cosine':
+            logging.info("Use scheduler cosine")
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs)
+            if args.warmup:
+                warmup_scheduler = warmup.UntunedLinearWarmup(optimizer)
+            else:
+                warmup_scheduler = None
+    else:
+        scheduler = None
+        warmup_scheduler = None
+    
+    ## load datas
+    datas = unify_dataset_loader(args.pre_train_datasets, args)
+    ## saved stats
+    best_avg_val = 0
+    val_accs = []
+    test_accs = []
+    
+
+    for e in range(max_epochs):
+        ## pre-train stage
+        for i, G in enumerate(datas):
+            train_loader = G.search(batch_size = batch_size, shuffle = True, time='train')
+            loss = train(model, optimizer, train_loader, args, scheduler, device, model_type = args.method, warmup_scheduler=warmup_scheduler)
+            logging.info("Epoch {} Dataset {} Loss {}".format(e, args.pre_train_datasets[i], loss))
+        ## evaluation part
+        eval_val = []
+        eval_test = []
+        for i, G in enumerate(datas):
+            test_loader = G.search(batch_size = batch_size, shuffle = False, time='test')
+
+            val_res, test_res = test(model, G, test_loader, args, device, level = DATASET[args.pre_train_datasets[i]]['level'])
+            eval_val.append(val_res)
+            eval_test.append(test_res)
+        for i in range(len(datas)):
+            logging.info("Epoch {} Dataset {} Val-{} {} Test-{} {}".format(e, args.downstream_datasets[i], DATASET[args.downstream_datasets[i]]['metric'], eval_val[i], DATASET[args.downstream_datasets[i]]['metric'], eval_test[i]))
+        avg_val = np.mean(eval_val)
+        if avg_val > best_avg_val:
+            best_avg_val = avg_val
+            val_accs = eval_val
+            test_accs = eval_test
+    logging.info("Best Avg Val {}".format(best_avg_val))
+    for i in range(len(datas)):
+        logging.info("Dataset: {} Best Val {} Test {}".format(args.pre_train_datasets[i], val_accs[i], test_accs[i]))
+    return best_avg_val, val_accs, test_accs    
+            
+
+
+
+
+
+    
+
+if __name__ == '__main__':
+    args = build_args()
+    print(args)
+    if not args.not_same_pretrain_downstream:
+        args.downstream_datasets = args.pre_train_datasets
+    main(args)
+    
+
+  
+        
+        
+    
+
+    
